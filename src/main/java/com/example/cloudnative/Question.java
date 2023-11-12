34d950cb949209cb7cdf9c9bca9d0168e47e40f7
heejae <hjhjhjhj3770@naver.com>
2023-11-12 18:55:18 +0900

    feat: Answer, Question 엔티티 추가

---
+++ src/main/java/com/example/cloudnative/Question.java
@@ -0,0 +1,27 @@
+package com.example.cloudnative;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+}
