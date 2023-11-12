4e2d68e2e7f920edc402e54a9b9ce5a99a2e74ce
heejae <hjhjhjhj3770@naver.com>
2023-11-12 18:58:17 +0900

    feat: Question과 Answer을 양방향 매핑
    하나의 질문에 여러 답변이 달릴 수 있는 상태로 만들어준다.

--- src/main/java/com/example/cloudnative/Question.java
+++ src/main/java/com/example/cloudnative/Question.java
@@ -1,11 +1,14 @@
 package com.example.cloudnative;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +27,7 @@ public class Question {
     private String content;
 
     private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
 }
