d158856daae09777fa45765973f963189e483857
heejae <hjhjhjhj3770@naver.com>
2023-11-17 09:02:54 +0900

    refactor: 작성일시 역순으로 조회하기

--- src/main/java/com/example/cloudnative/domain/Question.java
+++ src/main/java/com/example/cloudnative/domain/Question.java
@@ -1,6 +1,5 @@
 package com.example.cloudnative.domain;
 
-import com.example.cloudnative.domain.Answer;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- src/main/java/com/example/cloudnative/service/QuestionService.java
+++ src/main/java/com/example/cloudnative/service/QuestionService.java
@@ -3,12 +3,12 @@ package com.example.cloudnative.service;
 import com.example.cloudnative.domain.Question;
 import com.example.cloudnative.repository.QuestionRepository;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -18,10 +18,9 @@ public class QuestionService {
     private final QuestionRepository questionRepository;
 
     public Page<Question> getList(int page) {
-        Pageable pageable = PageRequest.of(page, 10);
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(Sort.Direction.DESC,"createDate"));
         return questionRepository.findAll(pageable);
     }
-
     public Question getQuestion(Integer id) {
         Optional<Question> question = questionRepository.findById(id);
         if (question.isPresent()) {
