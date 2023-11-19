a0907465c8c52fe9f3f9456d3e03c1612d16948e
heejae <hjhjhjhj3770@naver.com>
2023-11-19 10:18:56 +0900

    refactor: 회워가입할때 오류마다 출력하기

--- src/main/java/com/example/cloudnative/util/QuestionInit.java
+++ src/main/java/com/example/cloudnative/util/QuestionInit.java
@@ -13,23 +13,9 @@ import org.springframework.stereotype.Component;
 public class QuestionInit {
 
     private final QuestionService questionService;
-    private final QuestionRepository questionRepository;
 
     @PostConstruct
     public void init(){
-        Question q1 = new Question();
-        q1.setSubject("질문글입니다.");
-        q1.setContent("도커란 무엇인가?");
-        q1.setCreateDate(LocalDateTime.now());
-
-        Question q2 = new Question();
-        q2.setSubject("맨유분석글");
-        q2.setContent("텐하흐 종신");
-        q2.setCreateDate(LocalDateTime.now());
-
-        questionRepository.save(q1);
-        questionRepository.save(q2);
-
         for (int i = 1; i <= 300; i++) {
             String subject = String.format("테스트 데이터입니다:[%03d]", i);
             String content = "내용무";
