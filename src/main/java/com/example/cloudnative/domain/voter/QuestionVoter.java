04cdc5a30a46ae234792072d389dcbfe48e28216
heejae <hjhjhjhj3770@naver.com>
2023-11-21 11:17:56 +0900

    refactor: 주석 처리

--- src/main/java/com/example/cloudnative/domain/voter/QuestionVoter.java
+++ src/main/java/com/example/cloudnative/domain/voter/QuestionVoter.java
@@ -3,9 +3,7 @@ package com.example.cloudnative.domain.voter;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import lombok.AccessLevel;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
